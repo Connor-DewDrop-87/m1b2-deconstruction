3728c6fa7d7481b39fae7c0706d646d8cceecb66
connor <connor.almeyda@gmail.com>
2025-09-30 16:53:10 -0400

    Version 1

--- almeyda-connor-a2-project/almeyda-connor-a2-project/almeyda-connor-a2-project/Game.cs
+++ almeyda-connor-a2-project/almeyda-connor-a2-project/almeyda-connor-a2-project/Game.cs
@@ -11,7 +11,10 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-
+        Color Nightsky = new Color("#0f052d");
+        Color Blue = new Color("#203671");
+        Color lightBlue = new Color("#36868f");
+        Color Green = new Color("#5fc75d");
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -20,7 +23,7 @@ namespace MohawkGame2D
         {
             Window.SetSize(400, 400);
             Window.SetTitle("Starry Night");
-            Window.ClearBackground(Color.Black);
+            
         }
 
         /// <summary>
@@ -28,7 +31,25 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            
+            Window.ClearBackground(Nightsky);
+            DrawRowOfClouds(Blue, 275, 25, 15, 33);
+            DrawRowOfClouds(lightBlue, 325, 50, 7, 67);
+            DrawRowOfClouds(Green, 400, 75, 5, 100);
+            
+        }
+        void DrawRowOfClouds(Color cloudColor, int y, int r, int count, float space)
+        {
+            
 
+            Draw.FillColor = cloudColor;
+            Draw.LineSize = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+
+                Draw.Circle(space * i, y, r);
+            }
         }
     }
 
